e29a6d36c45a0545aaf6ca97b567336c587a44ae
Rhidian De Wit <rhidiandewit@gmail.com>
2024-03-25 16:17:29 +0100

    Remove last() and first(), add std::string_view::subview()

--- src/StringSlicing.h
+++ src/StringSlicing.h
@@ -6,15 +6,36 @@
 
 namespace stdproposal
 {
-    // functions should obviously be part of std::basic_string
+    // functions should obviously be part of std::basic_string and std::basic_string_view
     // these are now free functions to show off implementation
 
     // 1
-    std::string_view slice(const std::string& source, size_t start, size_t end)
+    std::string subview(const std::string& source, size_t start, size_t end)
     {
         if (start >= source.size())
         {
-            throw std::out_of_range{ "slice() > Start is out of range" };
+            throw std::out_of_range{ "subview() > Start is out of range" };
+        }
+
+        if (end > source.size())
+        {
+            end = source.size();
+        }
+
+        if (end < start)
+        {
+            end = start;
+        }
+
+        return std::string(source.data() + start, end - start);
+    }
+
+    // 2
+    std::string_view subview(const std::string_view source, size_t start, size_t end)
+    {
+        if (start >= source.size())
+        {
+            throw std::out_of_range{ "subview() > Start is out of range" };
         }
 
         if (end > source.size())
@@ -28,27 +49,5 @@ namespace stdproposal
         }
 
         return std::string_view(source.data() + start, end - start);
-    }
-
-    // 2
-    std::string_view first(const std::string& source, size_t count)
-    {
-        if (count >= source.size())
-        {
-            count = source.size();
-        }
-
-        return std::string_view(source.data(), count);
-    }
-
-    // 3
-    std::string_view last(const std::string& source, size_t count)
-    {
-        if (count >= source.size())
-        {
-            count = source.size();
-        }
-
-        return std::string_view(source.data() + (source.size() - count), count);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,53 +3,52 @@
 
 #include "StringSlicing.h"
 
-TEST_CASE("Testing slice(size_t start, size_t end)")
+TEST_CASE("Testing std::string::subview(size_t start, size_t end)")
 {
 	const std::string source{ "Hello World!" }; // source.size() == 12
 
-	REQUIRE(stdproposal::slice(source, 0, 5) == "Hello");
-	REQUIRE(stdproposal::slice(source, 6, 11) == "World");
-	REQUIRE(stdproposal::slice(source, 0, 12) == "Hello World!");
-	REQUIRE(stdproposal::slice(source, 0, 18) == "Hello World!");
-	REQUIRE(stdproposal::slice(source, 3, 15) == "lo World!");
+	REQUIRE(stdproposal::subview(source, 0, 5) == "Hello");
+	REQUIRE(stdproposal::subview(source, 6, 11) == "World");
+	REQUIRE(stdproposal::subview(source, 0, 12) == "Hello World!");
+	REQUIRE(stdproposal::subview(source, 0, 18) == "Hello World!");
+	REQUIRE(stdproposal::subview(source, 3, 15) == "lo World!");
 	
-	REQUIRE(stdproposal::slice(source, 0, 1) == "H");
-	REQUIRE(stdproposal::slice(source, 11, 12) == "!");
+	REQUIRE(stdproposal::subview(source, 0, 1) == "H");
+	REQUIRE(stdproposal::subview(source, 11, 12) == "!");
 
-	REQUIRE(stdproposal::slice(source, 11, 11) == "");
-	REQUIRE(stdproposal::slice(source, 0, 0) == "");
-	REQUIRE(stdproposal::slice(source, 6, 6) == "");
+	REQUIRE(stdproposal::subview(source, 11, 11) == "");
+	REQUIRE(stdproposal::subview(source, 0, 0) == "");
+	REQUIRE(stdproposal::subview(source, 6, 6) == "");
 
-	REQUIRE(stdproposal::slice(source, 5, 0) == "");
-	REQUIRE(stdproposal::slice(source, 5, 4) == "");
-	REQUIRE(stdproposal::slice(source, 11, 10) == "");
+	REQUIRE(stdproposal::subview(source, 5, 0) == "");
+	REQUIRE(stdproposal::subview(source, 5, 4) == "");
+	REQUIRE(stdproposal::subview(source, 11, 10) == "");
 
-	REQUIRE_THROWS_AS(stdproposal::slice(source, 12, 18), std::out_of_range);
-	REQUIRE_THROWS_AS(stdproposal::slice(source, 15, 18), std::out_of_range);
+	REQUIRE_THROWS_AS(stdproposal::subview(source, 12, 18), std::out_of_range);
+	REQUIRE_THROWS_AS(stdproposal::subview(source, 15, 18), std::out_of_range);
 }
 
-TEST_CASE("Testing first(size_t count)")
+TEST_CASE("Testing std::string_view::subview(size_t start, size_t end)")
 {
-	const std::string source{ "Hello World!" }; // source.size() == 12
+	const std::string_view source{ "Hello World!" }; // source.size() == 12
 
-	REQUIRE(stdproposal::first(source, 1) == "H");
-	REQUIRE(stdproposal::first(source, 3) == "Hel");
-	REQUIRE(stdproposal::first(source, 7) == "Hello W");
-	REQUIRE(stdproposal::first(source, 12) == "Hello World!");
-	REQUIRE(stdproposal::first(source, 16) == "Hello World!");
+	REQUIRE(stdproposal::subview(source, 0, 5) == "Hello");
+	REQUIRE(stdproposal::subview(source, 6, 11) == "World");
+	REQUIRE(stdproposal::subview(source, 0, 12) == "Hello World!");
+	REQUIRE(stdproposal::subview(source, 0, 18) == "Hello World!");
+	REQUIRE(stdproposal::subview(source, 3, 15) == "lo World!");
+	
+	REQUIRE(stdproposal::subview(source, 0, 1) == "H");
+	REQUIRE(stdproposal::subview(source, 11, 12) == "!");
 
-	REQUIRE(stdproposal::first(source, 0) == "");
-}
+	REQUIRE(stdproposal::subview(source, 11, 11) == "");
+	REQUIRE(stdproposal::subview(source, 0, 0) == "");
+	REQUIRE(stdproposal::subview(source, 6, 6) == "");
 
-TEST_CASE("Testing last(size_t count)")
-{
-	const std::string source{ "Hello World!" }; // source.size() == 12
+	REQUIRE(stdproposal::subview(source, 5, 0) == "");
+	REQUIRE(stdproposal::subview(source, 5, 4) == "");
+	REQUIRE(stdproposal::subview(source, 11, 10) == "");
 
-	REQUIRE(stdproposal::last(source, 1) == "!");
-	REQUIRE(stdproposal::last(source, 3) == "ld!");
-	REQUIRE(stdproposal::last(source, 7) == " World!");
-	REQUIRE(stdproposal::last(source, 12) == "Hello World!");
-	REQUIRE(stdproposal::last(source, 18) == "Hello World!");
-
-	REQUIRE(stdproposal::last(source, 0) == "");
+	REQUIRE_THROWS_AS(stdproposal::subview(source, 12, 18), std::out_of_range);
+	REQUIRE_THROWS_AS(stdproposal::subview(source, 15, 18), std::out_of_range);
 }
